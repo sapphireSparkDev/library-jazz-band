e2120468d7ddc2cb160d3d7e4140dbd63b1e81c3
Janae <janaemoring@gmail.com>
2024-11-05 06:59:08 -0500

    Remove home page in favor of landing page

--- libraryjazzfrontend/src/App.tsx
+++ libraryjazzfrontend/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import AboutPage from './pages/AboutPage'
-import Home from './pages/Home'
 
 function App() {
 
@@ -12,7 +11,6 @@ function App() {
         <NavBar/>
       </div>
       <div>
-        <Home/>
         <AboutPage/>
       </div>
     </div>
