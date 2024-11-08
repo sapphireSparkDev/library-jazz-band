360636b58b07f4bb618272967caa8b6d74147644
Janae <janaemoring@gmail.com>
2024-11-08 03:06:18 -0500

    Remove logo for time being

--- libraryjazzfrontend/src/components/NavBar.tsx
+++ libraryjazzfrontend/src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
-import LJB_Logo from "../assets/LJB_Logo.png";
 import "../styles/NavBar.css";
 
 const NavBar = () => {
@@ -10,9 +9,6 @@ const NavBar = () => {
       <nav className="bg-neutral-900 backdrop-filter backdrop-blur-lg bg-opacity-30 ">
         <div className="flex items-center space-x-16 w-screen px-4 h-1/6 justify-end">
           <div className="flex flex-col items-center justify-start ">
-            <Link to="/">
-              <img className="size-1/6 mt-4" src={LJB_Logo} alt="Library Jazz Band Logo" />
-            </Link>
           </div>
           <div className="flex space-x-6">
             <Link
