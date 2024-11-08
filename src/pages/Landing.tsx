ae2473a36ceba2c280fed754d5aa415ee4566ada
Janae <janaemoring@gmail.com>
2024-11-08 09:23:56 -0500

    Complete landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom";
+
 import "../styles/Landing.css";
+import arrow from "../lib/assets/arrowAmber.png";
 import image from "../lib/assets/bigBand.jpg";
-import { Button } from "@/components/ui/button";
 
 const Landing = () => {
   return (
     <div className="image-container">
       <img src={image} alt="Image" className="image" />
       <div className="text">
-        <h1>Library Jazz Band</h1>
-        <Button size="lg">Learn More</Button>
+        <h1 className="mb-80"> The Library Jazz Band</h1>
+        <Link
+          to="/about"
+          className="text-amber-400 hover:text-yellow-500 shadow-sm mt-10 font-serif flex flex-col items-center"
+        >
+          <span>Learn More</span>
+          <img src={arrow} alt="arrow down" className="size-10 animate-bounce" />
+        </Link>
       </div>
     </div>
   );
