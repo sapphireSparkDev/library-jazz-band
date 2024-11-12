f1c890640453f747379c6f4faa403d5e718a0fd2
Janae <janaemoring@gmail.com>
2024-11-12 08:23:12 -0500

    Formatting and styling for the events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -7,33 +7,39 @@ const Events = () => {
       <div className="absolute inset-y-0 left-10 top-16 bg-neutral-950/75 p-10 w-2/6 h-screen">
         <Card className="mb-6">
           <CardHeader>
-            <CardTitle>
+            <CardTitle className="font-roboto text-lg">
               <span>It's Showtime: LJB Takes on Broadway</span>
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <h2>Sunday, November 17 at 2pm</h2>
-            <span>
+            <h2 className="text-md text-cyan-600 font-bold">
+              Sunday, November 17 at 2pm
+            </h2>
+            <span className="text-sm">
               Greenburgh Public Library/ 300 Tarrytown Rd, Elmsford, NY 10523
             </span>
           </CardContent>
           <CardContent>
-            <h2>Sunday, November 24 at 2pm</h2>
-            <span>
+            <h2 className="text-md text-cyan-600 font-bold">
+              Sunday, November 24 at 2pm
+            </h2>
+            <span className="text-sm">
               Grinton I. Will Branch/1500 Central Park Avenue, Yonkers, NY 10710
             </span>
           </CardContent>
         </Card>
         <Card>
           <CardHeader>
-            <CardTitle>
+            <CardTitle className="font-roboto  text-lg">
               <span>Hitting the Low Notes</span>
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <h2>April 2025 [Date TBD]</h2>
+            <h2 className="text-md text-cyan-600 font-bold">
+              April 2025 [Date TBD]
+            </h2>
 
-            <p className="line-clamp-3">
+            <p className="line-clamp-7">
               This concert will feature instruments many audience members may
               not have seen in this setting before: the tuba and the baritone
               and bass saxophones. Joining us for this show will be tuba and
@@ -42,12 +48,6 @@ const Events = () => {
               Sam Rivers, Carla Bley, Gil Evans, Charlie Haden, Muhal Richard
               Abrams, Taj Mahal and many more.
             </p>
-          </CardContent>
-          <CardContent>
-            <h2>Sunday, November 24 at 2pm</h2>
-            <span>
-              Grinton I. Will Branch/1500 Central Park Avenue, Yonkers, NY 10710
-            </span>
           </CardContent>
         </Card>
       </div>
