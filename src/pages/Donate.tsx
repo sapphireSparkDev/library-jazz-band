c93faceadd3c03a1117c9c914ef007e60720ac63
Janae <janaemoring@gmail.com>
2024-11-12 05:49:47 -0500

    About page edits complete

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -7,10 +7,7 @@ import { buttonVariants } from "@/components/ui/button";
 const Donate = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1
-        className="text-amber-500 mb-10"
-        style={{ fontFamily: "Diplomatica SC" }}
-      >
+      <h1 className="text-amber-500 mb-10 font-roboto text-3xl">
         Support the LJB!
       </h1>
       <div className="flex flex-row justify-center items-center space-x-10 mb-10">
@@ -22,38 +19,41 @@ const Donate = () => {
         ></img>
       </div>
       <div className="flex flex-col justify-items-center w-3/6 items-center">
-        <p
-          className="text-center text-lg mb-16"
-          style={{ fontFamily: "Diplomatica SC" }}
-        >
-          ArtsWestchester will <b>MATCH</b> your donation from now until
-          December 10! Our library shows are free… but running the band is not!
-          We offer three original programs a year, rehearse year-round, and pay
-          our musicians a (very) modest honorarium. This is in many ways a labor
-          of love. So far, generous funding from ArtsWestchester has helped us
-          program our seasons in advance. The Yonkers Public Library has been an
-          outstanding partner, permitting us to rehearse there twice a month,
-          and helping to promote our shows. We want to keep growing! To add more
-          performances, invite guest artists, license new music, widely promote
-          our concert schedule, consider recording our music! To do this, we
-          need to find additional sources of support. If you would like to
-          support our work, please consider a donation. If the donation is
-          received by December 10th ArtsWestchester has agreed to match it!
+        <p className="text-center text-lg mb-16 font-roboto">
+          Our library shows are free, thanks to the generous support of
+          ArtsWestchester and the Yonkers Public Library, but running the band
+          is not! And we want to keep growing – adding more performances, more
+          guest artists, making a recording.
         </p>
-        <div className="flex flex-col items-center text-lg"  style={{ fontFamily: "Diplomatica SC" }}>
-          <h2>There are <b>two</b> steps:</h2>
-          <ol className="list-decimal">
-            <li>
-              Complete this form (or print out and mail this form to [address])
+        <span className="underline font-roboto text-md mb-10">
+          <strong>
+            Until December 10, ArtsWestchester will MATCH any donation you make.
+          </strong>
+        </span>
+        <div className="flex flex-col items-center text-lg">
+          <ol className="list-decimal list-inside font-robotoThin mb-10">
+            <h2 className=" font-roboto">To donate:</h2>
+            <li className="indent-10 ">Download & complete this form.</li>
+            <li className="indent-10">
+              {" "}
+              Email to maronnancy@gmail.com; or Mail to Nancy Maron 69 Oakland
+              Avenue/Yonkers, NY 10710
             </li>
-            <li>
-              We accept donations via
-              <ol className="list-disc list-inside indent-6">
-                <li>Check</li>
-                <li>Zelle</li>
+            <li className="indent-10">
+              <strong>Donations can be made via</strong>
+              <ol className="list-disc list-inside indent-20">
+                <li>
+                  Check (payable to Nancy Maron; send with form to 69 Oakland
+                  Avenue)
+                </li>
+                <li>Zelle (maronnancy@gmail.com)</li>
               </ol>
             </li>
-          </ol> 
+          </ol>
+          <span>
+            [Note: The Library Jazz Band is in the process of incorporating as a
+            not-for-profit organization but does not yet have nfp status.]
+          </span>
         </div>
       </div>
 
