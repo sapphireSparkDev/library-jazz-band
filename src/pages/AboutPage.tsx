e7377afa315bb603802974111519664f71de80a2
Janae <janaemoring@gmail.com>
2024-11-14 14:47:37 -0500

    Reconfigure About Page styles

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,7 +5,7 @@ import "../styles/About.css";
 
 const AboutPage = () => {
   return (
-    <div className="flex flex-col items-center w-screen" >
+    <div className="flex flex-col items-center w-screen">
       <div className="w-screen h-1/5 flex flex-col justify-center items-center">
         <h1 className="text-amber-500 mb-10 font-bebasNeue tracking-wide text-4xl">
           About Library Jazz Band
@@ -15,7 +15,10 @@ const AboutPage = () => {
             src={rob}
             className=" w-50 h-64 rounded-lg shadow-lg hidden lg:block"
           ></img>
-          <img src={kim} className=" w-25 h-32 rounded-lg shadow-lg lg:w-50 lg:h-64"></img>
+          <img
+            src={kim}
+            className=" w-25 h-32 rounded-lg shadow-lg lg:w-50 lg:h-64"
+          ></img>
           <img
             src={karl}
             className=" w-50 h-64 rounded-lg shadow-lg hidden lg:block"
@@ -26,7 +29,7 @@ const AboutPage = () => {
         <h2 className="font-bold text-xl mb-6 text-center">
           LJB: A Jazz Orchestra for the Community
         </h2>
-        <p className="text-center text-lg mb-10 font-roboto lg:w-fit w-screen">
+        <p className="text-center text-lg mb-14 font-roboto lg:w-fit w-screen">
           LJB was created in 2021 to address a gap in the musical landscape by
           reviving the big band tradition in their community. We are a dynamic
           17-piece jazz orchestra, featuring five saxophones, four trombones,
@@ -36,35 +39,43 @@ const AboutPage = () => {
           bringing vibrant, live jazz music to the community.
         </p>
       </div>
+      <div className="grid grid-cols-3 auto-rows-auto gap-x-6 justify-items-center text-center pl-5 pr-5">
+        <div className="flex flex-col">
+          <span className="font-roboto mb-10">
+            Giving musicians a place to keep growing as artists
+          </span>
+          <p className="font-robotoThin">
+            Our members don’t just play, they contribute their own arrangements
+            and compositions and we help local composers rehearse new works.{" "}
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-roboto mb-4">
+            Making America’s own music enjoyable and accessible to a wide
+            audience
+          </span>
+          <p className="font-robotoThin">
+            In addition to classic big band arrangements by the greats, we also
+            program some unexpected “jazz” adaptations such as tunes by Rolling
+            Stones and Stevie Wonder. Our summer 2024 show, BIG BAND: Small
+            Screen featured jazz renditions of music from cartoons and
+            television.{" "}
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-roboto mb-10 ">
+            Honoring the richness of jazz music and its history
+          </span>
 
-      <div className="grid lg:grid-rows-2 lg:grid-cols-3 md:auto-rows-auto md:grid-cols-1 h-20 mt-14 auto-cols-max justify-items-center gap-6 text-center font-roboto">
-        <span className="inline-block self-center font-bold lg:row-start-1 md:row-start-1 ">
-          Giving musicians a place to keep growing as artists
-        </span>
-        <span className="inline-block self-center font-bold lg:row-start-1 md:row-start-3 ">
-          Making America’s own music enjoyable and accessible to a wide audience
-        </span>
-        <span className="inline-block self-center  font-bold lg:row-start-1 md:row-start-5 ">
-          Honoring the richness of jazz music and its history
-        </span>
-        <p className="inline-block w-3/5 self-start font-robotoThin lg:row-start-2 md:row-start-2 ">
-          Our members don’t just play, they contribute their own arrangements
-          and compositions and we help local composers rehearse new works.{" "}
-        </p>
-        <p className="inline-block  w-3/5 font-robotoThin lg:row-start-2 md:row-start-4 ">
-          In addition to classic big band arrangements by the greats, we also
-          program some unexpected “jazz” adaptations such as tunes by Rolling
-          Stones and Stevie Wonder. Our summer 2024 show, BIG BAND: Small Screen
-          featured jazz renditions of music from cartoons and television.{" "}
-        </p>
-        <p className="inline-block  w-3/5 font-robotoThin lg:row-start-2 md:row-start-6 ">
-          Jazz is America’s very own music. Participating in keeping it alive
-          and growing is a wonderful way to remind all of us just how important
-          it is to keep instrumental music in our schools. Learning music is not
-          easy, but is a lifelong practice and a gift that should be accessible
-          to everyone. This music provides an accessible point of entry for
-          listeners of all ages and backgrounds.
-        </p>
+          <p className="font-robotoThin">
+            Jazz is America’s very own music. Participating in keeping it alive
+            and growing is a wonderful way to remind all of us just how
+            important it is to keep instrumental music in our schools. Learning
+            music is not easy, but is a lifelong practice and a gift that should
+            be accessible to everyone. This music provides an accessible point
+            of entry for listeners of all ages and backgrounds.
+          </p>
+        </div>
       </div>
     </div>
   );
