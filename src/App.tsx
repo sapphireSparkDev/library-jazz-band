831238506289eaba832a2d34a9a5363f921633b7
Janae <janaemoring@gmail.com>
2024-11-14 21:14:54 -0500

    Add footer to site

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Landing from "./pages/Landing";
 import Donate from "./pages/Donate";
 import AboutPage from "./pages/AboutPage";
 import Events from "./pages/Events";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/donate" element={<Donate />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </div>
     </>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center justify-center h-1/5 font-roboto text-amber-400">
+      <h1 className="mb-4 mt-4">2024 Library Jazz Band</h1>
+      <Link className="mb-4" to="https://lp.constantcontactpages.com/sl/Fx2eGn6">
+        We have a newsletter! Sign Up Here.
+      </Link>
+      <div className="flex flex-row space-x-4">
+        <Button variant="outline" size="icon">
+          <Link to="https://www.instagram.com/thelibraryjazzband">
+            <FaFacebookSquare />
+          </Link>
+        </Button>
+        <Button variant="outline" size="icon">
+          <Link to="https://www.instagram.com/thelibraryjazzband">
+            <FaInstagram />
+          </Link>
+        </Button>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
