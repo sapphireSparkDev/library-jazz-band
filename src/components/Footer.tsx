279822c4e7a92375d0279511379e449fa64b1f36
Janae <janaemoring@gmail.com>
2024-11-15 11:42:41 -0500

    Responsive updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center h-1/5 font-roboto text-amber-400">
       <h1 className="mb-4 mt-4">2024 Library Jazz Band</h1>
-      <Link className="mb-4" to="https://lp.constantcontactpages.com/sl/Fx2eGn6">
+      <Link className="mb-4" to="https://www.facebook.com/libraryjazzband">
         We have a newsletter! Sign Up Here.
       </Link>
       <div className="flex flex-row space-x-4">
         <Button variant="outline" size="icon">
-          <Link to="https://www.instagram.com/thelibraryjazzband">
+          <Link to="">
             <FaFacebookSquare />
           </Link>
         </Button>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -10,7 +10,7 @@ const Landing = () => {
     <div className="grid">
       <div className="inline-block bg-neutral-900 h-screen relative pr-6 pl-6 pb-8">
         <picture>
-          <source srcSet={danAsher} media="(max-width: 600px)" />
+          <source srcSet={danAsher} media="(max-width: 1024px)" />
           <img
             src={image}
             alt="The entire library jazz band"
@@ -19,13 +19,13 @@ const Landing = () => {
         </picture>
 
         <div className="text">
-          <h1 className=" font-aboreto text-5xl mb-28">
+          <h1 className="font-aboreto text-5xl  2xl:text-8xl mb-32 md:mb-40 lg:mb-40 xl:mb-12">
             {" "}
             The Library Jazz Band
           </h1>
           <Link
             to="/about"
-            className="text-amber-400 hover:text-yellow-500 shadow-sm mb-14 font-serif flex flex-col items-center"
+            className="text-amber-400 hover:text-yellow-500 shadow-sm mb-14 font-serif flex flex-col items-center 2xl:hidden"
           >
             <div className="md:mt-56 lg:mt-96">
               <span className="btn btn-text-stretch btn-text-stretch--orange">
