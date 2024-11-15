c3bf0d2c432e6fbec82c2148b86084790f029a0c
Janae <janaemoring@gmail.com>
2024-11-15 11:50:32 -0500

    Fix footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center h-1/5 font-roboto text-amber-400">
       <h1 className="mb-4 mt-4">2024 Library Jazz Band</h1>
-      <Link className="mb-4" to="https://www.facebook.com/libraryjazzband">
+      <Link className="mb-4" to=" https://lp.constantcontactpages.com/sl/Fx2eGn6">
         We have a newsletter! Sign Up Here.
       </Link>
       <div className="flex flex-row space-x-4">
         <Button variant="outline" size="icon">
-          <Link to="">
+          <Link to="https://www.facebook.com/libraryjazzband">
             <FaFacebookSquare />
           </Link>
         </Button>
